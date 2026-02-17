b819bf3717bc60b53a060127ab758b83a6cc5836
Adharsh510 <aadhiadharsh098@gmail.com>
2026-02-17 10:59:01 +0530

    python basics

---
+++ beginning.py/begining/firstst.py
@@ -0,0 +1,28 @@
+a=100
+a=float(a)
+print(type(a))  
+print(a)
+
+num=5+2.0
+print(num)
+print(type(num))    
+
+num_int=(5.7)
+num_int=int(num_int)
+print(type(num_int))
+print(num_int)
+
+num_float=float("3.14")
+print(type(num_float))
+print(num_float)
+
+message= str(123)
+print(type(message))
+print(message)
+
+is_true= bool(1)
+is_false= bool(0)
+print(is_true)
+print(is_false)
+print(type(is_true))
+print(type(is_false))
---
+++ loops.py
@@ -0,0 +1,31 @@
+#marks =int(input("enter a number: "))
+#if marks >=75:
+       # print("distintion")
+#elif marks>=60:
+        #print("first class")
+#elif marks>=35:
+        #print("Pass")
+#elif marks<=34:
+        #print("Fail")   
+
+
+#for i in range(1,6):
+        # print(i)
+#n=int(input("enter number: "))
+#for i in range(1,11):
+        #print(n, "X", i, "=", n*i)
+
+#i=1
+#while i<=5:
+   #     print(i)
+ #       i+=1   
+
+#n= int(input("enter number: "))
+#i=1
+#sum =0
+#while i<= n:
+ #       sum = sum+i
+  #      i+=1
+#print("sum = ", sum)   
+
+print ("Hello Nnanna")
